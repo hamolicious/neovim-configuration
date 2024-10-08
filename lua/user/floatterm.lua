c280d741ef1e3d6791572ca9bd807710807426a3
hamolicious <hamolicious@hamolicious.net>
2024-10-08 14:43:09 +0100

    add spt integration

--- lua/user/floatterm.lua
+++ lua/user/floatterm.lua
@@ -1,3 +1,4 @@
 
 vim.keymap.set('n', '<leader><leader>l', '<cmd>FloatermNew --title=LazyGit --autoclose=2 lazygit<cr>', {desc = 'Open lazygit'})
+vim.keymap.set('n', '<leader><leader>s', '<cmd>FloatermNew --title=Spotify --autoclose=2 spt<cr>', {desc = 'Open lazygit'})
 
