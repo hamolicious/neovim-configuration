c324493dcc4861275ddd6a3215ff2ac76f2790c0
hamolicious <hamolicious@hamolicious.net>
2024-10-08 14:52:02 +0100

    add docker integration

--- lua/user/floatterm.lua
+++ lua/user/floatterm.lua
@@ -1,4 +1,5 @@
 
 vim.keymap.set('n', '<leader><leader>l', '<cmd>FloatermNew --title=LazyGit --autoclose=2 lazygit<cr>', {desc = 'Open lazygit'})
 vim.keymap.set('n', '<leader><leader>s', '<cmd>FloatermNew --title=Spotify --autoclose=2 spt<cr>', {desc = 'Open lazygit'})
+vim.keymap.set('n', '<leader><leader>d', '<cmd>FloatermNew --title=Docker --autoclose=2 lazydocker<cr>', {desc = 'Open lazydocker'})
 
