51f6928684d17ec15a815f62b788798bfc71467a
Viktors Patmalnieks <viktorspatmalnieks@comparethemarket.com>
2024-10-10 13:01:49 +0100

    adding notes on more scroll binds

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,4 +1,6 @@
 -- Keybinds
+-- C-e  |  scroll down a bit
+-- C-y  |  scroll up a bit
 -- C-u  |  up
 -- C-d  |  down
 -- C-f  |  page-down
@@ -11,15 +13,15 @@ return {
 	'karb94/neoscroll.nvim',
 	config = function()
 		require('neoscroll').setup({
-			hide_cursor = true,      -- Hide cursor while scrolling
-			stop_eof = true,         -- Stop at <EOF> when scrolling downwards
+			hide_cursor = true,       -- Hide cursor while scrolling
+			stop_eof = true,          -- Stop at <EOF> when scrolling downwards
 			respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
 			cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-			easing = 'sine',         -- Default easing function
-			pre_hook = nil,          -- Function to run before the scrolling animation starts
-			post_hook = nil,         -- Function to run after the scrolling animation ends
-			performance_mode = true, -- Disable "Performance Mode" on all buffers.
-			ignored_events = {       -- Events ignored while scrolling
+			easing = 'sine',          -- Default easing function
+			pre_hook = nil,           -- Function to run before the scrolling animation starts
+			post_hook = nil,          -- Function to run after the scrolling animation ends
+			performance_mode = true,  -- Disable "Performance Mode" on all buffers.
+			ignored_events = {        -- Events ignored while scrolling
 				'WinScrolled', 'CursorMoved'
 			},
 		})
