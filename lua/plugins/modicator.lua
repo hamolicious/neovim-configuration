5996d6d2748d517d5fec0adbdf706a4c05b2748a
hamolicious <hamolicious@hamolicious.net>
2024-10-11 08:01:01 +0100

    fix config

--- lua/plugins/modicator.lua
+++ lua/plugins/modicator.lua
@@ -1,18 +1,17 @@
 return {
 	'mawkler/modicator.nvim',
 	event = 'ModeChanged',
-	init = function()
-		vim.o.cursorline = true
-		vim.o.number = true
-		vim.o.termguicolors = true
-	end,
 	integration = {
 		lualine = {
 			enabled = true,
-			-- Letter of lualine section to use (if `nil`, gets detected automatically)
-			mode_section = 'a',
-			-- Whether to use lualine's mode highlight's foreground or background
 			highlight = 'bg',
 		}
-	}
+	},
+	config = function()
+		vim.o.cursorline = true
+		vim.o.number = true
+		vim.o.termguicolors = true
+
+		require('modicator').setup()
+	end,
 }
