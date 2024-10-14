9aff9166fc77e0c441d585217cc1b7b545e8a8ad
hamolicious <hamolicious@hamolicious.net>
2024-10-14 06:54:16 +0100

    add descriptions

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -10,13 +10,14 @@ return {
 		require('snippets.python')
 
 		-- vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-		vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true, desc = "12345" })
-		vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true, desc = "Jump to next insertion" })
+		vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end,
+			{ silent = true, desc = "Jump to previous insertion" })
 
 		vim.keymap.set({ "i", "s" }, "<C-E>", function()
 			if ls.choice_active() then
 				ls.change_choice(1)
 			end
-		end, { silent = true })
+		end, { silent = true, desc = "Cycle throught insertions" })
 	end,
 }
