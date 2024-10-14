7ea11537733ea575182b2b30917185fc04a8319e
hamolicious <hamolicious@hamolicious.net>
2024-10-14 06:39:31 +0100

    add snippet keybinds

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -5,7 +5,18 @@ return {
 	},
 	after = 'nvim-cmp',
 	config = function()
+		local ls = require('luasnip')
 		require('snippets.global')
 		require('snippets.python')
+
+		-- vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true, desc = "12345" })
+		vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-E>", function()
+			if ls.choice_active() then
+				ls.change_choice(1)
+			end
+		end, { silent = true })
 	end,
 }
