9df956d7dcebd71079a92be0f1e9e144153851eb
hamolicious <hamolicious@hamolicious.net>
2024-10-14 08:36:19 +0100

    slight clean up

--- lua/plugins/auto-complete.lua
+++ lua/plugins/auto-complete.lua
@@ -11,18 +11,13 @@ return {
 
 		cmp.setup({
 			snippet = {
-				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
-					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-					vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+					vim.snippet.expand(args.body)       -- For native neovim snippets (Neovim v0.10+)
 				end,
 			},
 			window = {
 				completion = cmp.config.window.bordered(),
-				-- documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -33,25 +28,11 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-				{ name = 'vsnip' }, -- For vsnip users.
-				{ name = 'luasnip' }, -- For luasnip users.
-				-- { name = 'ultisnips' }, -- For ultisnips users.
-				-- { name = 'snippy' }, -- For snippy users.
-			}, {
-				{ name = 'buffer' },
+				{ name = 'nvim_lua' },
+				{ name = 'luasnip' },
+				{ name = 'buffer',  keyword_length = 3 },
 			})
 		})
-
-		-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-		-- Set configuration for specific filetype.
-		--[[ cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'git' },
-	}, {
-		{ name = 'buffer' },
-	})
-})
-require("cmp_git").setup() ]] --
 
 		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 		cmp.setup.cmdline({ '/', '?' }, {
