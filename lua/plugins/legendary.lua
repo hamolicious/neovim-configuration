2de8b964267ce2628e2c6527489dedaaf834502e
hamolicious <hamolicious@hamolicious.net>
2024-10-18 22:41:04 +0100

    fix typo

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -15,7 +15,6 @@ return {
 				-- Telescope
 				{ '<leader>ff',        ':Telescope find_files<cr>',                                                                                              description = 'Telescope find files' },
 				{ '<leader>fr',        ':Telescope registers<cr>',                                                                                               description = 'Telescope find registers' },
-				{ '<leader>fr',        ':Telescope treesitter<cr>',                                                                                              description = 'Telescope find registers' },
 				{ '<leader>fg',        ':Telescope live_grep<cr>',                                                                                               description = 'Telescope find with live grep' },
 				{ '<leader>fh',        ':Telescope help_tags<cr>',                                                                                               description = 'Telescope find help tags' },
 				{ '<leader>fd',        ':Telescope diagnostics<cr>',                                                                                             description = 'Telescope find diagnostics' },
