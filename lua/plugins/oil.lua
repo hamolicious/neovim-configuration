520cdcbae53bb6b0281d8577c310891365f22385
hamolicious <hamolicious@hamolicious.net>
2024-10-18 22:02:50 +0100

    misc settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -12,6 +12,8 @@ return {
 	config = function()
 		local oil = require("oil")
 		oil.setup({
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = false,
 			view_options = {
 				show_hidden = true,
 			}
