49772fd55aa0d66af0ad36fba4a8f0ec3eb4d5a5
Viktors Patmalnieks <viktorspatmalnieks@comparethemarket.com>
2025-01-17 10:57:59 +0000

    toggling bools, months, weekdays and yes/nos

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -30,33 +30,34 @@ end
 -- install plugins
 lazy.setup({
   spec = {
-    require('plugins.legendary'),          -- Keybinds, auto-cmds and arbitrary funcs
-    require('plugins.floaterm'),           -- Floating terminals
-    require('plugins.lsp'),                -- Intelisense
-    require('plugins.formatting'),         -- Formatting
-    require('plugins.linting'),            -- Formatting
-    require('plugins.dresser'),            -- Makes legendary bareable to look at
-    require('plugins.theme'),              -- Theme
-    require('plugins.lualine'),            -- Lualine
-    require('plugins.telescope'),          -- Telescope
-    require('plugins.auto-complete'),      -- nvim-cmp
-    require('plugins.git'),                -- git-tui
-    require('plugins.tree-sitter'),        -- tree-sitter
-    require('plugins.highlight'),          -- highligh occurances
-    require('plugins.neoscroll'),          -- smooth scrolling
-    require('plugins.visual-whitespace'),  -- visually display whitespaces
-    require('plugins.comment'),            -- visually display whitespaces
-    require('plugins.oil'),                -- File namagement as buffer
-    require('plugins.fold-line'),          -- Show folds
-    require('plugins.auto-scale-windows'), -- Scale windows automatically
-    require('plugins.minesweeper'),        -- minesweeper, why not
-    require('plugins.modicator'),          -- Dynamically & automatically scale windows
-    require('plugins.snippets'),           -- Dynamic snippets
-    require('plugins.gitlinker'),          -- Quickly copy permalinks to git
-    require('plugins.coloriser'),          -- Preview hex codes
-    require('plugins.chatgpt'),            -- chatgpt integration
-    require('plugins.autoclose'),          -- automatically close brackets
-    require('plugins.markdown-preview'),   -- Live markdown preview
+    require('plugins.legendary'),           -- Keybinds, auto-cmds and arbitrary funcs
+    require('plugins.floaterm'),            -- Floating terminals
+    require('plugins.lsp'),                 -- Intelisense
+    require('plugins.formatting'),          -- Formatting
+    require('plugins.linting'),             -- Formatting
+    require('plugins.dresser'),             -- Makes legendary bareable to look at
+    require('plugins.theme'),               -- Theme
+    require('plugins.lualine'),             -- Lualine
+    require('plugins.telescope'),           -- Telescope
+    require('plugins.auto-complete'),       -- nvim-cmp
+    require('plugins.git'),                 -- git-tui
+    require('plugins.tree-sitter'),         -- tree-sitter
+    require('plugins.highlight'),           -- highligh occurances
+    require('plugins.neoscroll'),           -- smooth scrolling
+    require('plugins.visual-whitespace'),   -- visually display whitespaces
+    require('plugins.comment'),             -- visually display whitespaces
+    require('plugins.oil'),                 -- File namagement as buffer
+    require('plugins.fold-line'),           -- Show folds
+    require('plugins.auto-scale-windows'),  -- Scale windows automatically
+    require('plugins.minesweeper'),         -- minesweeper, why not
+    require('plugins.modicator'),           -- Dynamically & automatically scale windows
+    require('plugins.snippets'),            -- Dynamic snippets
+    require('plugins.gitlinker'),           -- Quickly copy permalinks to git
+    require('plugins.coloriser'),           -- Preview hex codes
+    require('plugins.chatgpt'),             -- chatgpt integration
+    require('plugins.autoclose'),           -- automatically close brackets
+    require('plugins.markdown-preview'),    -- Live markdown preview
+    require('plugins.increment-decrement'), -- Toggle bools, weekdays, on off and yes, no
 
   },
   ---@diagnostic disable-next-line
---
+++ lua/plugins/increment-decrement.lua
@@ -0,0 +1,24 @@
+return {
+  'nat-418/boole.nvim',
+  config = function()
+    require('boole').setup({
+      mappings = {
+        increment = '<C-a>',
+        decrement = '<C-x>'
+      },
+      additions = {
+        {
+          'car',
+          'van',
+          'bike',
+          'home',
+          'travel',
+          'loans',
+          'creditcards',
+          'pet',
+          'life',
+        },
+      },
+    })
+  end
+}
