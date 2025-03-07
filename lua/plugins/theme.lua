f0c659f0516f0821185c9e9cb6ea6f8ba4020d5b
hamolicious <hamolicious@hamolicious.net>
2025-03-07 12:37:17 +0000

    fix vert split

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,18 @@
 return {
-	'folke/tokyonight.nvim',
-	config = function()
-		vim.cmd.colorscheme('tokyonight-night')
-	end
+  'folke/tokyonight.nvim',
+  config = function()
+    vim.cmd.colorscheme('tokyonight-night')
+
+    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#565f89" })
+
+    vim.opt.fillchars:append({
+      horiz = '━',
+      horizup = '┻',
+      horizdown = '┳',
+      vert = '┃',
+      vertleft = '┫',
+      vertright = '┣',
+      verthoriz = '╋',
+    })
+  end
 }
