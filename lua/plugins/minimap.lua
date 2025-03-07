b3fddd7ff0dadec554231341ffe7dfe69a460198
hamolicious <hamolicious@hamolicious.net>
2025-03-07 12:39:21 +0000

    more sane binds for minimap

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -13,18 +13,6 @@ return {
     { "<leader>nc",  "<cmd>Neominimap off<cr>",         desc = "Disable global minimap" },
     { "<leader>nr",  "<cmd>Neominimap refresh<cr>",     desc = "Refresh global minimap" },
 
-    -- Window-Specific Minimap Controls
-    { "<leader>nwt", "<cmd>Neominimap winToggle<cr>",   desc = "Toggle minimap for current window" },
-    { "<leader>nwr", "<cmd>Neominimap winRefresh<cr>",  desc = "Refresh minimap for current window" },
-    { "<leader>nwo", "<cmd>Neominimap winOn<cr>",       desc = "Enable minimap for current window" },
-    { "<leader>nwc", "<cmd>Neominimap winOff<cr>",      desc = "Disable minimap for current window" },
-
-    -- Tab-Specific Minimap Controls
-    { "<leader>ntt", "<cmd>Neominimap tabToggle<cr>",   desc = "Toggle minimap for current tab" },
-    { "<leader>ntr", "<cmd>Neominimap tabRefresh<cr>",  desc = "Refresh minimap for current tab" },
-    { "<leader>nto", "<cmd>Neominimap tabOn<cr>",       desc = "Enable minimap for current tab" },
-    { "<leader>ntc", "<cmd>Neominimap tabOff<cr>",      desc = "Disable minimap for current tab" },
-
     -- Buffer-Specific Minimap Controls
     { "<leader>nbt", "<cmd>Neominimap bufToggle<cr>",   desc = "Toggle minimap for current buffer" },
     { "<leader>nbr", "<cmd>Neominimap bufRefresh<cr>",  desc = "Refresh minimap for current buffer" },
@@ -34,7 +22,7 @@ return {
     ---Focus Controls
     { "<leader>nf",  "<cmd>Neominimap focus<cr>",       desc = "Focus on minimap" },
     { "<leader>nu",  "<cmd>Neominimap unfocus<cr>",     desc = "Unfocus minimap" },
-    { "<leader>ns",  "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
+    { "<leader>nt",  "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
   },
   init = function()
     local min_num_lines = 30
