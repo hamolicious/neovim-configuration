8ddf72231d0dba91d31dd0bc4129454b639c0a51
hamolicious <hamolicious@hamolicious.net>
2025-03-08 02:44:42 +0000

    switch http clients

--- lua/plugins/http-client.lua
+++ lua/plugins/http-client.lua
@@ -1,84 +1,21 @@
+-- TODO: lualine is fighting http for winbar
+
 return {
-  {
-    'mistweaverco/kulala.nvim',
-    ft = { "http" },
-    opts = {
-      scratchpad_default_contents = {
-        "@MY_TOKEN_NAME=my_token_value",
-        "",
-        "# @name scratchpad",
-        "POST https://httpbin.org/post HTTP/1.1",
-        "accept: application/json",
-        "content-type: application/json",
-        "",
-        "{",
-        '  "foo": "bar"',
-        "}",
-      },
-    },
-    config = function()
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "<CR>",
-        "<cmd>lua require('kulala').run()<cr>",
-        { noremap = true, silent = true, desc = "Execute the request" }
-      )
-
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "{",
-        "<cmd>lua require('kulala').jump_prev()<cr>",
-        { noremap = true, silent = true, desc = "Jump to the previous request" }
-      )
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "}",
-        "<cmd>lua require('kulala').jump_next()<cr>",
-        { noremap = true, silent = true, desc = "Jump to the next request" }
-      )
-
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "<leader>i",
-        "<cmd>lua require('kulala').inspect()<cr>",
-        { noremap = true, silent = true, desc = "Inspect the current request" }
-      )
-
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "<leader>t",
-        "<cmd>lua require('kulala').toggle_view()<cr>",
-        { noremap = true, silent = true, desc = "Toggle between body and headers" }
-      )
-
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "<leader>co",
-        "<cmd>lua require('kulala').copy()<cr>",
-        { noremap = true, silent = true, desc = "Copy the current request as a curl command" }
-      )
-
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "<leader>ci",
-        "<cmd>lua require('kulala').from_curl()<cr>",
-        { noremap = true, silent = true, desc = "Paste curl from clipboard as http request" }
-      )
-
-      vim.api.nvim_buf_set_keymap(
-        0,
-        "n",
-        "<leader>ci",
-        "<cmd>lua require('kulala').from_curl()<cr>",
-        { noremap = true, silent = true, desc = "Paste curl from clipboard as http request" }
-      )
-    end
+  "rodrigoscc/http.nvim",
+  config = function()
+    require("http-nvim").setup({
+      http_dir = "~/.http",
+      hooks_file = "hooks.lua",
+      environments_file = "environments.json",
+      active_envs_file = vim.fn.stdpath("data") .. "/http/envs.json",
+      win_config = { split = "below" },
+    })
+  end,
+  build = { ":TSUpdate http2", ":Http update_grammar_queries" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim",
+    "ibhagwan/fzf-lua",
   },
 }
