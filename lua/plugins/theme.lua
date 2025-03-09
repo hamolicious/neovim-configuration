275441e62d2fd342d0b7b03fba4888ff7b7eb5e5
hamolicious <hamolicious@hamolicious.net>
2025-03-09 16:15:55 +0000

    fix TODO highlighting

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,16 @@ return {
 
     vim.api.nvim_set_hl(0, "WinSeparator", { fg = CustomColors.SPLIT })
 
+    vim.cmd([[
+      augroup MyTODO
+        autocmd!
+        autocmd Syntax * call matchadd('myTodo', '\v<(HACK|FIX|QUESTION|TODO|NOTE)>\:')
+      augroup END
+      highlight myTodo ctermbg=yellow ctermfg=black guibg=yellow guifg=black
+    ]])
+
+    vim.api.nvim_set_hl(0, "TODO", { fg = 'Yellow' })
+
     vim.opt.fillchars:append({
       horiz = '━',
       horizup = '┻',
