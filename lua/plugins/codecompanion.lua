f313bb747d94917f5dbb6ec2bb67193c1bd80805
Viktors Patmalnieks <viktorspatmalnieks@comparethemarket.com>
2025-03-11 09:30:04 +0000

    explicit display def

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -6,6 +6,19 @@ return {
   },
   config = function()
     require("codecompanion").setup({
+      display = {
+        chat = {
+          show_header_separator = false,
+          auto_scroll = true,
+        },
+      },
+      diff = {
+        enabled = true,
+        close_chat_at = 240,  -- Close an open chat buffer if the total columns of your display are less than...
+        layout = "vertical",  -- vertical|horizontal split for default provider
+        opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+        provider = "default", -- default|mini_diff
+      },
       strategies = {
         chat = {
           adapter = "openai",
