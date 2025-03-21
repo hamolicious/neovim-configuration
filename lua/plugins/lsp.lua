7759832fd3d0c2fb68c555269b6eddc7d5c9458a
hamolicious <hamolicious@hamolicious.net>
2025-03-21 14:02:33 +0000

    remove ast grep

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,34 +1,32 @@
 return {
-	'neovim/nvim-lspconfig',
-	dependencies = {
-		{ 'williamboman/mason.nvim' },
-		{ 'williamboman/mason-lspconfig.nvim' },
-	},
-	config = function()
-		require('mason').setup()
-		require('mason-lspconfig').setup({
-			ensure_installed = {
-				'pyright',
-				'lua_ls',
-				'ts_ls',
-				'ast_grep',
-				-- not a valid entry?!
-				-- 'eslint_d',
-				-- 'prettierd',
-				-- 'eslint_d',
-				-- 'black',
-				-- 'isort',
-			}
-		})
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+  },
+  config = function()
+    require('mason').setup()
+    require('mason-lspconfig').setup({
+      ensure_installed = {
+        'pyright',
+        'lua_ls',
+        'ts_ls',
+        -- not a valid entry?!
+        -- 'eslint_d',
+        -- 'prettierd',
+        -- 'eslint_d',
+        -- 'black',
+        -- 'isort',
+      }
+    })
 
-		require('lspconfig').lua_ls.setup({})
-		require('lspconfig').pyright.setup({})
-		require('lspconfig').ts_ls.setup({})
-		require('lspconfig').csharp_ls.setup({})
-		require('lspconfig').docker_compose_language_service.setup({})
-		require('lspconfig').dockerls.setup({})
-		require('lspconfig').gitlab_ci_ls.setup({})
-		require('lspconfig').ast_grep.setup({})
-	end
+    require('lspconfig').lua_ls.setup({})
+    require('lspconfig').pyright.setup({})
+    require('lspconfig').ts_ls.setup({})
+    require('lspconfig').csharp_ls.setup({})
+    require('lspconfig').docker_compose_language_service.setup({})
+    require('lspconfig').dockerls.setup({})
+    require('lspconfig').gitlab_ci_ls.setup({})
+  end
 
 }
