e2a2d7083fe4a23d3924f9395d8ec48a86680d7f
hamolicious <hamolicious@hamolicious.net>
2025-04-13 23:07:15 +0100

    fix oil C-h/l

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -17,9 +17,30 @@ return {
       delete_to_trash = true,
       skip_confirm_for_simple_edits = false,
       watch_for_changes = true,
+
       view_options = {
         show_hidden = true,
-      }
+      },
+
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["L"] = "actions.refresh",
+        ["<C-l>"] = {},
+        ["<C-h>"] = {},
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
     })
 
     vim.keymap.set(
