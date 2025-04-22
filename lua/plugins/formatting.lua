cf5d7774959cc9cb94e966cf29e2bcf9c425859f
Viktors Patmalnieks <viktorspatmalnieks@comparethemarket.com>
2025-04-22 08:44:05 +0100

    remove clang

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -7,7 +7,6 @@ return {
     local setupTable = {
       formatters_by_ft = {
         python = { 'isort', 'black' },
-        c = { 'ast_grep', 'clang-format' },
         javascript = { 'prettierd' },
         typescript = { 'prettierd' },
         css = { 'prettierd' },
