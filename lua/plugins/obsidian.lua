7476c1352a5323ff93f41ace0eecc830f0054ba7
Viktors Patmalnieks <viktorspatmalnieks@comparethemarket.com>
2025-04-25 16:45:42 +0100

    use absolute path to dailies

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -30,7 +30,7 @@ return {
       -- Optional, default tags to add to each new daily note created.
       default_tags = nil,
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = 'Templates/Daily Note Template.md'
+      template = '~/.notes/Notes/Templates/Daily Note Template.md'
     },
 
     templates             = {
