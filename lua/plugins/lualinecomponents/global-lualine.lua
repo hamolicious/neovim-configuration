e342a86bb8b755d7b92be8851ce8c0bff7056df8
hamolicious <hamolicious@hamolicious.net>
2025-04-26 02:02:34 +0100

    move tabs down

--- lua/plugins/lualinecomponents/global-lualine.lua
+++ lua/plugins/lualinecomponents/global-lualine.lua
@@ -1,7 +1,21 @@
+local tab_component = {
+  'tabs',
+  mode       = 1,
+  tabs_color = {
+    active = { fg = CustomColors.BG, bg = CustomColors.ACTIVE, gui = 'bold' },
+    inactive = { fg = CustomColors.TEXT, bg = CustomColors.BG },
+  },
+  fmt        = function(name, context)
+    local number = context.tabnr
+    return '  ' .. number .. ' - ' .. name .. ' '
+  end,
+  max_length = function() return vim.o.columns end,
+}
+
 local lualine_config = {
   lualine_a = { 'mode' },
   lualine_b = { 'branch' },
-  lualine_c = { 'diff', 'diagnostics' },
+  lualine_c = { 'diff', 'diagnostics', tab_component },
   lualine_x = {
     require("plugins.codecompanion.lualine"),
     require("http-nvim").http_env_lualine_component,
