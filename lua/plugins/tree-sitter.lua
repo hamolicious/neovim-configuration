9ca4953942ca5f24c1caf02a04dacbcf3a716604
hamolicious <hamolicious@hamolicious.net>
2025-04-26 12:20:46 +0100

    add markdown to ts

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,23 +1,35 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	config = function()
-		require("lazy").setup({ {
-			"nvim-treesitter/nvim-treesitter",
-			build = ":TSUpdate",
-			config = function()
-				local configs = require("nvim-treesitter.configs")
+  'nvim-treesitter/nvim-treesitter',
+  config = function()
+    require("lazy").setup({ {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+        local configs = require("nvim-treesitter.configs")
 
-				configs.setup({
-					ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "html", "c_sharp" },
-					ignore_install = { "help", "man" },  -- Add the file types you want to ignore
-					sync_install = false,
-					highlight = {
-						enable = true,
-						disable = { "help", "man" },  -- Disable Treesitter highlighting for help and man pages
-					},
-					indent = { enable = true },
-				})
-			end
-		} })
-	end
+        configs.setup({
+          ensure_installed = {
+            "c",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "python",
+            "javascript",
+            "html",
+            "c_sharp",
+            "markdown",
+            "markdown_inline",
+          },
+          ignore_install = { "help", "man" }, -- Add the file types you want to ignore
+          sync_install = true,
+          highlight = {
+            enable = true,
+            disable = { "help", "man" }, -- Disable Treesitter highlighting for help and man pages
+          },
+          indent = { enable = true },
+        })
+      end
+    } })
+  end
 }
