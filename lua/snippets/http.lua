c74cdbd846c885c81f6b83f291d2759856973180
hamolicious <hamolicious@hamolicious.net>
2025-04-27 18:16:03 +0100

    make snippets work for http again

--- lua/snippets/http.lua
+++ lua/snippets/http.lua
@@ -11,74 +11,75 @@ local func = ls.function_node
 local choice = ls.choice_node
 local dynamicn = ls.dynamic_node
 
+local http = {
+  snip({
+      trig = "request",
+      namr = "New Request",
+      dscr = "Create new basic request"
+    },
+    {
+      text({
+        "@request.title=JSON",
+        "POST https://example.hehe",
+        "User-Agent: test-request",
+        "Accept: application/json",
+        "Content-Type: application/json",
+        "",
+        "{",
+        "",
+        "}",
+      }),
+    }
+  ),
+
+
+
+  snip({
+      trig = "title",
+      namr = "Set request title",
+      dscr = "Request title"
+    },
+    {
+      text('@request.title = '),
+      insert(1, 'Request Title')
+    }),
+
+
+  snip({
+      trig = "after_hook",
+      namr = "request.after_hook",
+      dscr = "Request after hook"
+    },
+    {
+      text('@request.after_hook = '),
+      insert(1, 'save_access_token')
+    }),
+
+  snip({
+      trig = "before_hook",
+      namr = "request.before_hook",
+      dscr = "Request before hook"
+    },
+    {
+      text('@request.before_hook = '),
+      insert(1, 'ask_for_confirmation')
+    }),
+
+  snip({
+      trig = "#",
+      namr = "Request split",
+      dscr = "Insert `###` request splitter"
+    },
+    {
+      text({
+        '',
+        '###',
+        ''
+      }),
+    }),
+}
 
 ls.add_snippets(nil, {
-  http = {
-    snip({
-        trig = "request",
-        namr = "New Request",
-        dscr = "Create new basic request"
-      },
-      {
-        text({
-          "@request.title=JSON",
-          "POST https://example.hehe",
-          "User-Agent: test-request",
-          "Accept: application/json",
-          "Content-Type: application/json",
-          "",
-          "{",
-          "",
-          "}",
-        }),
-      }
-    ),
-
-
-
-    snip({
-        trig = "title",
-        namr = "Set request title",
-        dscr = "Request title"
-      },
-      {
-        text('@request.title = '),
-        insert(1, 'Request Title')
-      }),
-
-
-    snip({
-        trig = "after_hook",
-        namr = "request.after_hook",
-        dscr = "Request after hook"
-      },
-      {
-        text('@request.after_hook = '),
-        insert(1, 'save_access_token')
-      }),
-
-    snip({
-        trig = "before_hook",
-        namr = "request.before_hook",
-        dscr = "Request before hook"
-      },
-      {
-        text('@request.before_hook = '),
-        insert(1, 'ask_for_confirmation')
-      }),
-
-    snip({
-        trig = "#",
-        namr = "Request split",
-        dscr = "Insert `###` request splitter"
-      },
-      {
-        text({
-          '',
-          '###',
-          ''
-        }),
-      }),
-
-  }
+  http = http,
+  http2 = http,
 })
